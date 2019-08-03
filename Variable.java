adfb89a2dbfadb6d311397f9556c2fd318e4b819
narasakp <narasakp@gmail.com>
2019-08-03 14:19:26 +0700

    Variable.java

---
+++ Variable.java
@@ -0,0 +1,18 @@
+public class Variable {
+    public static void main(String[] args){
+        int number = 10;
+        long longNumber = 100;
+        double doubleNumber = 1.5;
+        float floatNumber = 30.0f;
+        char charText = 'C';
+        String Text = "Message";
+
+        System.out.println(number);
+        System.out.println(longNumber);
+        System.out.println(doubleNumber);
+        System.out.println(floatNumber);
+        System.out.println(charText);
+        System.out.println(Text);
+    } 
+}
+

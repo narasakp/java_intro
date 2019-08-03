f4405eaeb8b8f690d372f1c39e2f54b363da8248
narasakp <narasakp@gmail.com>
2019-08-03 15:37:49 +0700

    ForLoop.java

---
+++ ForLoop.java
@@ -0,0 +1,15 @@
+public class ForLoop{
+    public static void main(String[] args) {
+        System.out.println("Increse");
+        for (int i = 0; i <= 10; i++) {
+            System.out.println("The value of i is " + i);
+        }
+        
+        System.out.println("Decrese");
+        for (int i = 10; i >= 0; i--) {
+            System.out.println(i);
+        }
+    }
+
+        
+}

74d82708009b716037661192e2c5633641f26809
narasakp <narasakp@gmail.com>
2019-08-03 15:08:08 +0700

    Operator.java

--- Operator.java
+++ Operator.java
@@ -10,13 +10,28 @@ public class Operator {
         System.out.println(num2/num1);
 
         String msg = "hello";
-        //System.out.println(num1+msg);
+        System.out.println(num1+msg);
 
         if (num1 > num2) {
             System.out.println("num1 > num2");
         }
             else {
                 System.out.println("num1 < num2");
+        }
+
+        if (num1==10) {
+                System.out.println("num1=10");
+        } 
+
+        if (num2!=10){
+                System.out.println("num2!=10");
+        }
+
+        String text1 = "text";
+        String text2 = "text";
+
+        if (text1.equals(text2)){
+            System.out.println("text1==text2");
         }
     }
 }
